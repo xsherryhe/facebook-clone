1e4e694beb7c7d6f09e424341d8e439e2f6bc99e
Sherry He <xsherryhe@gmail.com>
2022-09-22 14:07:39 -0400

    Added feature to remove and add photos multiple times when creating Post or Comment

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,34 +2,87 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['input', 'preview', 'submit'];
+  static values = { fileFieldInd: Number };
 
-  reset(e) {
+  reset() {
     this.element.reset();
     this.previewTarget.textContent = '';
+    this.setSubmit();
+  }
+
+  updateFileDisplay(e) {
+    this.updatePreview(e, '150', true);
+    this.replaceFileField();
   }
 
   replacePreviewLarge(e) {
-    this.updatePreview(e, 0, '250');
+    this.updatePreview(e, '250', false);
   }
 
   replacePreviewSmall(e) {
-    this.updatePreview(e, 0, '150');
+    this.updatePreview(e, '150', false);
   }
 
-  addToPreview(e) {
-    this.updatePreview(e, 1, '150');
-  }
-
-  updatePreview(e, type, height) {
-    if(type == 0)
-      this.previewTarget.textContent = '';
-    const imageFiles = [...e.target.files];
-    imageFiles.forEach(file => {
-      const image = document.createElement('img');
-      if(height) image.height = height;
-      image.src = URL.createObjectURL(file);
-      this.previewTarget.appendChild(image);
+  //TO DO: Figure out how to remove images that are already in database?
+  updatePreview(e, height, multiple) {
+    [...e.target.files].forEach((file, i) => {
+      const container = this._createContainer(i);
+      container.appendChild(this._createImg(file, height));
+      if(multiple) container.appendChild(this._createRemoveLink());
+      multiple ? this.previewTarget.appendChild(container) : this.previewTarget.replaceChildren(container);
     })
+  }
+
+  _createContainer(imageInd) {
+    const container = document.createElement('div');
+    container.id = `filefield-${this.fileFieldIndValue}-image-${imageInd}`;
+    return container;
+  }
+
+  _createImg(file, height) {
+    const image = document.createElement('img');
+    if(height) image.height = height;
+    image.src = URL.createObjectURL(file);
+    return image;
+  }
+
+  _createRemoveLink() {
+    const link = document.createElement('a');
+    link.href = '';
+    link.textContent = 'Remove';
+    link.setAttribute('data-action', 'form#removeFile');
+    return link;
+  }
+
+  replaceFileField() {
+    const oldFileField = this.element.querySelector(`#filefield-${this.fileFieldIndValue}`),
+          newFileField = oldFileField.cloneNode();
+    oldFileField.classList.add('hidden');
+    newFileField.id = `filefield-${++this.fileFieldIndValue}`;
+    newFileField.value = '';
+    this.element.querySelector('.photo-upload').appendChild(newFileField);
+  }
+
+  removeFile(e) {
+    e.preventDefault();
+    const container = e.target.parentNode,
+          [fileInd, imageInd] = container.id.split('-').filter(segment => /\d/.test(segment)).map(Number),
+          fileField = this.element.querySelector(`#filefield-${fileInd}`),
+          oldFiles = [...fileField.files],
+          newFiles = new DataTransfer();
+
+    oldFiles.forEach((file, i) => {
+      if(i !== imageInd) newFiles.items.add(file);
+    })
+    fileField.files = newFiles.files;
+    
+    container.remove();
+    for(let i = imageInd; i < oldFiles.length - 1; i++) {
+      const followingContainer = this.element.querySelector(`#filefield-${fileInd}-image-${i + 1}`);
+      followingContainer.id = `#filefield-${fileInd}-image-${i}`;
+    }
+
+    this.setSubmit();
   }
 
   setSubmit() {
