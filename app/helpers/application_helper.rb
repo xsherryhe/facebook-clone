870e66bdfa5a793a4160c8cc69de15e2a8ee7b09
Sherry He <xsherryhe@gmail.com>
2022-09-25 17:18:20 -0400

    Began styling index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def mini_avatar(profile, link_path = user_path(profile.user))
-    render profile.avatar, link_path:, height: '35'
+    render profile.avatar, link_path:, height: '40'
   end
 
   def full_avatar(profile, link_path = image_path(profile.avatar))
