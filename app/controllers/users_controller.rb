a60394860982344be0a39d07d6e5164b3a389782
Sherry He <xsherryhe@gmail.com>
2022-09-29 21:42:53 -0400

    Additional minor modifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
                             comments: [{ user: :profile }, :comments, { photos: { stored_attachment: :blob } }],
                             likes: { user: :profile })
                   .order(updated_at: :desc)
+                  .limit(10)
     @photos = @user.photos.with_attached_stored.limit(4)
   end
 end
