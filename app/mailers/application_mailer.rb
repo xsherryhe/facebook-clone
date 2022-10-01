5b77c67c27240f8094a2e0ed16b1204e394204de
Sherry He <xsherryhe@gmail.com>
2022-09-30 23:15:14 -0400

    Completed styling of mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  before_action :set_logo_attachment
+
+  private
+
+  def set_logo_attachment
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
+  end
 end
