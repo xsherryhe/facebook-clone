2c45afc03de43db8a7fa4e41cee9e6e204ca80fd
Sherry He <xsherryhe@gmail.com>
2022-10-06 17:45:32 -0400

    Connected app with Mailgun for email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'sleepy-springs-52383.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'sleepy-springs-52383.heroku.com',
+    authentication: :plain
+  }
 end
